484276d4835b312b17e1cc5838b2680d4378d15a
Roehit2001 <roehitr1@gmail.com>
2021-08-24 10:11:42 +0530

    google analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-VMTE6JWZQ2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
